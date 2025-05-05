dfa488aa462932e46f44fddf6677ff22f1244c22
Muki Kiboigo <muki@muki.gg>
2025-05-05 12:57:30 -0700

    fix: remove url check

--- default.nix
+++ default.nix
@@ -1,138 +1,142 @@
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs ? import <nixpkgs> { },
   system ? builtins.currentSystem,
-}: let
+}:
+let
   inherit (pkgs) lib;
   sources = lib.importJSON ./sources.json;
 
-  urlsForFile = file: let
-    # This is a list of known Zig nightly mirrors used by https://github.com/mlugg/setup-zig.
-    # File hashes are exactly the same so `fetchurl` can try them in order.
-    mirrors = lib.importJSON ./mirrors.json;
-  in
-    [("https://ziglang.org/builds/" + file)]
+  urlsForFile =
+    file:
+    let
+      # This is a list of known Zig nightly mirrors used by https://github.com/mlugg/setup-zig.
+      # File hashes are exactly the same so `fetchurl` can try them in order.
+      mirrors = lib.importJSON ./mirrors.json;
+    in
+    [ ("https://ziglang.org/builds/" + file) ]
     ++ map (mirror: (builtins.elemAt mirror 0) + "/" + file) mirrors;
 
   # mkBinaryInstall makes a derivation that installs Zig from a binary.
-  mkBinaryInstall = {
-    file ? null,
-    url ? null,
-    broken ? false,
-    version,
-    sha256,
-  }:
+  mkBinaryInstall =
+    {
+      file ? null,
+      url ? null,
+      broken ? false,
+      version,
+      sha256,
+    }:
     assert file == null -> url != null;
-      pkgs.stdenv.mkDerivation (finalAttrs: {
-        pname = "zig";
-        inherit version;
+    pkgs.stdenv.mkDerivation (finalAttrs: {
+      pname = "zig";
+      inherit version;
 
-        src = pkgs.fetchurl {
-          inherit sha256;
-          urls = urlsForFile (
-            if file != null
-            then file
-            # Backwards compatibility with old sources.json
-            else (lib.removePrefix "https://ziglang.org/builds/" url)
-          );
-        };
+      src = pkgs.fetchurl {
+        inherit sha256;
+        urls = urlsForFile (
+          if file != null then
+            file
+          # Backwards compatibility with old sources.json
+          else
+            (lib.removePrefix "https://ziglang.org/builds/" url)
+        );
+      };
 
-        dontConfigure = true;
-        dontBuild = true;
-        dontFixup = true;
+      dontConfigure = true;
+      dontBuild = true;
+      dontFixup = true;
 
-        installPhase = ''
-          mkdir -p $out/{doc,bin,lib}
-          [ -d docs ] && cp -r docs/* $out/doc
-          [ -d doc ] && cp -r doc/* $out/doc
-          cp -r lib/* $out/lib
-          cp zig $out/bin/zig
-        '';
+      installPhase = ''
+        mkdir -p $out/{doc,bin,lib}
+        [ -d docs ] && cp -r docs/* $out/doc
+        [ -d doc ] && cp -r doc/* $out/doc
+        cp -r lib/* $out/lib
+        cp zig $out/bin/zig
+      '';
 
-        passthru = {
-          hook = pkgs.zig.hook.override {zig = finalAttrs.finalPackage;};
-          zls = let
+      passthru = {
+        hook = pkgs.zig.hook.override { zig = finalAttrs.finalPackage; };
+        zls =
+          let
             versionMap = lib.importJSON ./zls-versions.json;
             zlsVersion =
-              if (builtins.hasAttr version versionMap)
-              then versionMap.${version}.version
-              else throw "";
+              if (builtins.hasAttr version versionMap) then versionMap.${version}.version else throw "";
           in
-            zlsPackages.${"zls-" + zlsVersion};
-        };
+          zlsPackages.${"zls-" + zlsVersion};
+      };
 
-        meta = with lib; {
-          description = "General-purpose programming language and toolchain for maintaining robust, optimal, and reusable software";
-          homepage = "https://ziglang.org/";
-          license = licenses.mit;
-          maintainers = [];
-          platforms = platforms.unix;
-          inherit broken;
-        };
-      });
+      meta = with lib; {
+        description = "General-purpose programming language and toolchain for maintaining robust, optimal, and reusable software";
+        homepage = "https://ziglang.org/";
+        license = licenses.mit;
+        maintainers = [ ];
+        platforms = platforms.unix;
+        inherit broken;
+      };
+    });
 
   # The packages that are tagged releases
-  taggedPackages =
-    lib.attrsets.mapAttrs
-    (k: v: mkBinaryInstall v.${system})
-    (lib.attrsets.filterAttrs
-      (k: v:
+  taggedPackages = lib.attrsets.mapAttrs (k: v: mkBinaryInstall v.${system}) (
+    lib.attrsets.filterAttrs
+      (
+        k: v:
         (builtins.hasAttr system v)
-        && (v.${system}.url != null)
+        # && (v.${system}.url != null)
         && (v.${system}.sha256 != null)
-        && !(lib.strings.hasSuffix "mach" k))
-      (builtins.removeAttrs sources ["master" "mach-latest"]));
+        && !(lib.strings.hasSuffix "mach" k)
+      )
+      (
+        builtins.removeAttrs sources [
+          "master"
+          "mach-latest"
+        ]
+      )
+  );
 
   # The master packages
   masterPackages =
-    lib.attrsets.mapAttrs' (
-      k: v:
-        lib.attrsets.nameValuePair
-        (
-          if k == "latest"
-          then "master"
-          else v.${system}.version
+    lib.attrsets.mapAttrs'
+      (
+        k: v:
+        lib.attrsets.nameValuePair (if k == "latest" then "master" else v.${system}.version) (
+          mkBinaryInstall v.${system}
         )
-        (mkBinaryInstall v.${system})
-    )
-    (lib.attrsets.filterAttrs
-      (k: v:
-        (builtins.hasAttr system v)
-        && (builtins.hasAttr "url" v.${system})
-        && (v.${system}.url != null))
-      sources.master);
+      )
+      (
+        lib.attrsets.filterAttrs (
+          k: v:
+          (builtins.hasAttr system v) && (builtins.hasAttr "url" v.${system}) && (v.${system}.url != null)
+        ) sources.master
+      );
 
   # Mach nominated versions
   # https://machengine.org/docs/nominated-zig/
-  machPackages =
-    lib.attrsets.mapAttrs
-    (k: v: mkBinaryInstall v.${system})
-    (lib.attrsets.filterAttrs (k: v: lib.strings.hasSuffix "mach" k)
-      (builtins.removeAttrs sources ["master"]));
+  machPackages = lib.attrsets.mapAttrs (k: v: mkBinaryInstall v.${system}) (
+    lib.attrsets.filterAttrs (k: v: lib.strings.hasSuffix "mach" k) (
+      builtins.removeAttrs sources [ "master" ]
+    )
+  );
 
   # This determines the latest /released/ version.
   latest = lib.lists.last (
-    builtins.sort
-    (x: y: (builtins.compareVersions x y) < 0)
-    (builtins.attrNames taggedPackages)
+    builtins.sort (x: y: (builtins.compareVersions x y) < 0) (builtins.attrNames taggedPackages)
   );
 
   # Latest Mach nominated version
   machLatest = lib.lists.last (
-    builtins.sort
-    (x: y: (builtins.compareVersions x y) < 0)
-    (builtins.attrNames machPackages)
+    builtins.sort (x: y: (builtins.compareVersions x y) < 0) (builtins.attrNames machPackages)
   );
 
-  mkBinaryZls = {
-    url,
-    sha256,
-    version,
-  }:
+  mkBinaryZls =
+    {
+      url,
+      sha256,
+      version,
+    }:
     pkgs.stdenv.mkDerivation {
       pname = "zls";
       inherit version;
 
-      src = pkgs.fetchurl {inherit url sha256;};
+      src = pkgs.fetchurl { inherit url sha256; };
       sourceRoot = ".";
 
       dontConfigure = true;
@@ -145,47 +149,41 @@
       '';
     };
 
-  zlsPackages = let
-    sources = lib.importJSON ./zls-sources.json;
-    zigSystem =
-      if lib.hasSuffix system "darwin"
-      then (lib.removeSuffix "darwin") + "macos"
-      else system;
-  in
-    lib.mapAttrs' (n: v:
-      lib.nameValuePair
-      ("zls-" + n)
-      (mkBinaryZls {
+  zlsPackages =
+    let
+      sources = lib.importJSON ./zls-sources.json;
+      zigSystem = if lib.hasSuffix system "darwin" then (lib.removeSuffix "darwin") + "macos" else system;
+    in
+    lib.mapAttrs' (
+      n: v:
+      lib.nameValuePair ("zls-" + n) (mkBinaryZls {
         inherit (v) version;
         url = v.${zigSystem}.tarball;
         sha256 = v.${zigSystem}.shasum;
-      }))
-    sources;
+      })
+    ) sources;
 
   zlsLatest = lib.lists.last (
-    builtins.sort
-    (x: y: (builtins.compareVersions x y) < 0)
-    (builtins.filter (v: !lib.hasInfix "dev" v) (builtins.attrNames zlsPackages))
+    builtins.sort (x: y: (builtins.compareVersions x y) < 0) (
+      builtins.filter (v: !lib.hasInfix "dev" v) (builtins.attrNames zlsPackages)
+    )
   );
 
   zlsMaster = lib.lists.last (
-    builtins.sort
-    (x: y: (builtins.compareVersions x y) < 0)
-    (builtins.attrNames zlsPackages)
+    builtins.sort (x: y: (builtins.compareVersions x y) < 0) (builtins.attrNames zlsPackages)
   );
 in
-  # We want the packages but also add a "default" that just points to the
-  # latest released version.
-  lib.mapAttrs' (k: v: lib.nameValuePair (lib.replaceStrings ["." "+"] ["_" "_"] k) v)
-  (
-    taggedPackages
-    // masterPackages
-    // machPackages
-    // zlsPackages
-    // {
-      "default" = taggedPackages.${latest};
-      mach-latest = machPackages.${machLatest};
-      zls-latest = zlsPackages.${zlsLatest};
-      zls-master = zlsPackages.${zlsMaster};
-    }
-  )
+# We want the packages but also add a "default" that just points to the
+# latest released version.
+lib.mapAttrs' (k: v: lib.nameValuePair (lib.replaceStrings [ "." "+" ] [ "_" "_" ] k) v) (
+  taggedPackages
+  // masterPackages
+  // machPackages
+  // zlsPackages
+  // {
+    "default" = taggedPackages.${latest};
+    mach-latest = machPackages.${machLatest};
+    zls-latest = zlsPackages.${zlsLatest};
+    zls-master = zlsPackages.${zlsMaster};
+  }
+)
--- flake.nix
+++ flake.nix
@@ -12,45 +12,56 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    outputs = flake-utils.lib.eachSystem systems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
-      # The packages exported by the Flake:
-      #  - default - latest /released/ version
-      #  - <version> - tagged version
-      #  - master - latest nightly (updated daily)
-      #  - master-<date> - nightly by date
-      packages = import ./default.nix {inherit system pkgs;};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      outputs = flake-utils.lib.eachSystem systems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          # The packages exported by the Flake:
+          #  - default - latest /released/ version
+          #  - <version> - tagged version
+          #  - master - latest nightly (updated daily)
+          #  - master-<date> - nightly by date
+          packages = import ./default.nix { inherit system pkgs; };
 
-      # "Apps" so that `nix run` works. If you run `nix run .` then
-      # this will use the latest default.
-      apps = rec {
-        default = apps.zig;
-        zig = flake-utils.lib.mkApp {drv = packages.default;};
-      };
+          # "Apps" so that `nix run` works. If you run `nix run .` then
+          # this will use the latest default.
+          apps = rec {
+            default = apps.zig;
+            zig = flake-utils.lib.mkApp { drv = packages.default; };
+          };
 
-      # nix fmt
-      formatter = pkgs.alejandra;
+          # nix fmt
+          formatter = pkgs.alejandra;
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          curl
-          jq
-          minisign
-        ];
-      };
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              curl
+              jq
+              minisign
+            ];
+          };
 
-      # For compatibility with older versions of the `nix` binary
-      devShell = self.devShells.${system}.default;
-    });
-  in
+          # For compatibility with older versions of the `nix` binary
+          devShell = self.devShells.${system}.default;
+        }
+      );
+    in
     outputs
     // {
       # Overlay that can be imported so you can access the packages
